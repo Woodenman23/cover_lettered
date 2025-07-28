6098f4b9bf65a8e1f828f3e1830ca5935bc4fba8
Joe Foster <joe.foster@protonmail.com>
2025-07-28 14:36:21 -0700

    set-up

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
+
 def create_app() -> Flask:
     app = Flask(__name__)
 
     from website.views import views
+
     app.register_blueprint(views, url_prefix="/")
 
     return app
